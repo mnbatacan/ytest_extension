5f109fd68ab76af738db90e0fb001e20a29de4fd
maureen <mnbatacan@up.edu.ph>
2018-10-25 14:12:41 +0800

    Pop-up tabs form. No stats yet

--- inject.js
+++ inject.js
@@ -19,7 +19,7 @@
 
 // document.getElementById.apply(document, arguments);
 // }
-
+src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
 var div = document.createElement( 'div' );
 var btn = document.createElement( 'input' );
 document.body.appendChild( div );
@@ -33,12 +33,13 @@ div.style.left = '75%';
 // div.style.right = '70%';
 div.style.width = '90%';   
 div.style.height = '100%';
-div.style.backgroundColor = 'red';
+div.style.background = 'rgba(228,238,255,0.8)';
+// div.style.opacity = '0.7';
 
 
 
 btn.type = 'button';
-btn.id = 'puta';
+btn.id = 'moderateBtn';
 btn.value = 'Moderate!';
 // btn.style.position = 'absolute';
 btn.style.visibility = 'visible';
@@ -50,4 +51,6 @@ btn.style.marginLeft = "10px";
 btn.style.marginTop = "14px";
 // btn.style.top = '50%';
 // btn.style.left = '50%';
-btn.style.backgroundColor = 'yellow';
+btn.class="waves-effect waves-light btn-large";
+btn.style.backgroundColor = '#3765AB';
+btn.style.color = 'white';
