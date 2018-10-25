af720f2a3861dc480eceaec1168a4af2a42c4e15
maureen <mnbatacan@up.edu.ph>
2018-10-25 09:00:13 +0800

    fixed UI injecetd elements

--- inject.js
+++ inject.js
@@ -24,24 +24,30 @@ var div = document.createElement( 'div' );
 var btn = document.createElement( 'input' );
 document.body.appendChild( div );
 div.appendChild( btn );
+div.appendChild( title);
 div.id = 'myDivId';
 div.style.position = 'fixed';
 div.style.alignContent='center';
-div.style.top = '50%';
-div.style.left = '50%';
-div.style.width = '100%';   
+div.style.top = '90%';
+div.style.left = '75%';
+// div.style.right = '70%';
+div.style.width = '90%';   
 div.style.height = '100%';
 div.style.backgroundColor = 'red';
 
 
+
 btn.type = 'button';
 btn.id = 'puta';
-btn.value = 'hello';
+btn.value = 'Moderate!';
 // btn.style.position = 'absolute';
 btn.style.visibility = 'visible';
 btn.style.zIndex='1000';
-btn.style.height = '20%';
-btn.style.width = '20%';
-btn.style.top = '50%';
-btn.style.left = '50%';
+btn.style.height = '5%';
+btn.style.width = '26%';
+
+btn.style.marginLeft = "10px";
+btn.style.marginTop = "14px";
+// btn.style.top = '50%';
+// btn.style.left = '50%';
 btn.style.backgroundColor = 'yellow';
